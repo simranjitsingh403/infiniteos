53a59e08b36588f6c10621d86237c2a89cfe436c
PrikshitKsa <prikshitk@sharparchtechnologies.com>
2025-06-07 16:40:55 +0530

    animation enabled

--- infiniteos/Program.cs
+++ infiniteos/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.StaticFiles;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -14,6 +16,14 @@ if (!app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+var provider = new FileExtensionContentTypeProvider();
+provider.Mappings[".glb"] = "model/gltf-binary";
+
+app.UseStaticFiles(new StaticFileOptions
+{
+    ContentTypeProvider = provider
+});
+
 app.UseStaticFiles();
 
 app.UseRouting();
