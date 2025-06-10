1fa9cc2038d16421bf28862d41dcca01a4ea12ed
simranjeetsa <simranjeetsa@gmail.com>
2025-06-10 12:53:51 +0530

    changes

--- infiniteos/wwwroot/js/site.js
+++ infiniteos/wwwroot/js/site.js
@@ -107,3 +107,31 @@ document.querySelectorAll('.cursor-dots').forEach(el => {
         el.style.setProperty('--y', '50%');
     });
 });
+
+
+const slider = document.querySelector('.cus-slider-container');
+let isDown = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener('mousedown', (e) => {
+    isDown = true;
+  
+    startX = e.pageX - slider.offsetLeft;
+    scrollLeft = slider.scrollLeft;
+});
+slider.addEventListener('mouseleave', () => {
+    isDown = false;
+    
+});
+slider.addEventListener('mouseup', () => {
+    isDown = false;
+    
+});
+slider.addEventListener('mousemove', (e) => {
+    if (!isDown) return;
+    e.preventDefault();
+    const x = e.pageX - slider.offsetLeft;
+    const walk = (x - startX) * 1.5; // scroll speed
+    slider.scrollLeft = scrollLeft - walk;
+});
