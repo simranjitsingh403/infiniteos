01e60d074354e5bdf7f7e4fed9f49085fd4aff8e
simranjeetsa <simranjeetsa@gmail.com>
2025-06-12 17:45:54 +0530

    changes

--- infiniteos/Controllers/Industries.cs
+++ infiniteos/Controllers/Industries.cs
@@ -8,5 +8,33 @@ namespace infiniteos.Controllers
         {
             return View();
         }
+        public IActionResult Pharmaceuticals()
+        {
+            return View();
+        }
+        public IActionResult CallCenterAndCustomerService()
+        {
+            return View();
+        }
+        public IActionResult SupplyChain()
+        {
+            return View();
+        }
+        public IActionResult SkilledTrades()
+        {
+            return View();
+        }
+        public IActionResult Manufacturing()
+        {
+            return View();
+        }
+        public IActionResult OfficeAndClericalAdministration()
+        {
+            return View();
+        }
+        public IActionResult Warehousing()
+        {
+            return View();
+        }
     }
 }
